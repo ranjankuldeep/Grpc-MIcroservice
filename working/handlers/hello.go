6f08ea85326400692486170873c866af04e47cbe
ranjankuldeep <ranjankuldeep19@gmail.com>
2023-09-11 12:51:20 +0530

    Code refractoring

---
+++ working/handlers/hello.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+func (h *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.l.Println("Hello World")
+	// r.Body has i/o reader interface
+	// use ioutil for reading these streams from i/o interface
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "oops", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "hello %s\n", d)
+}
--- working/main.go
+++ working/main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+	"working/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		// r.Body has i/o reader interface
-		// use ioutil for reading these streams from i/o interface
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "oops", http.StatusBadRequest)
-			return
-		}
-		log.Printf("Data %s\n", d)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
-		fmt.Fprintf(w, "hello %s\n", d)
-	})
-	http.ListenAndServe(":8080", nil)
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	// graceful shutdown
+	signChan := make(chan os.Signal)
+	signal.Notify(signChan, os.Interrupt)
+	signal.Notify(signChan, os.Kill)
+
+	// block until signal is received
+	sig := <-signChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
+	defer cancel()
+	s.Shutdown(tc)
 }
