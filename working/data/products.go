2ab485d3add895f423b9ccb473a28608daf5e9bf
ranjankuldeep <ranjankuldeep19@gmail.com>
2023-09-12 21:26:23 +0530

    Post Method added as rest principle architecture

--- working/data/products.go
+++ working/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"time"
 )
@@ -24,9 +25,20 @@ func (p *Products) ToJSON(w http.ResponseWriter) error {
 	//This below will Encode method will mutate the p pointer receiver to JSON and write to the ResponseWriter interface w passed in as a parameter to the ToJSON method.
 	return e.Encode(p)
 }
-
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
 func GetProducts() Products {
 	return productList
+}
+func AddProduct(p *Product) {
+	p.ID = getNextId()
+	productList = append(productList, p)
+}
+func getNextId() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 // create a slice of products
--- working/handlers/products.go
+++ working/handlers/products.go
@@ -17,19 +17,19 @@ func NewProducts(l *log.Logger) *Products {
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// handle get request
 	if r.Method == http.MethodGet {
-		p.GetProducts(w, r)
+		p.getProducts(w, r)
 		return
 	}
 	// handle post request
 	if r.Method == http.MethodPost {
-		p.AddProduct(w, r)
+		p.addProduct(w, r)
 		return
 	}
 	// catch all
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(w)
 	if err != nil {
@@ -37,6 +37,16 @@ func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-func (p *Products) AddProduct(w http.ResponseWriter, r *http.Request) {
+func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST Product")
+
+	// Below is a pointer to a Product struct that is empty
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to unmarshal JSON", http.StatusBadRequest)
+	}
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
 }
